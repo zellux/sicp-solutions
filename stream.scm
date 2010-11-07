480b609d084b183baf58fc56b37bc4abbf7c09c0
Wang Yuanxuan <zellux@gmail.com>
2010-11-07 16:57:16 +0800

    3.67

--- stream.scm
+++ stream.scm
@@ -100,7 +100,13 @@
 (define pi-stream
   (scale-stream (partial-sums (pi-summands 1)) 4))
 
-(display-stream pi-stream)
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))           ; Sn-1
+        (s1 (stream-ref s 1))           ; Sn
+        (s2 (stream-ref s 2)))          ; Sn+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
 
 (define (make-tableau transform s)
   (cons-stream s
@@ -111,3 +117,18 @@
   (stream-map stream-car
               (make-tableau transform s)))
 
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(display-stream (pairs (stream-enumerate-interval 0 10) (stream-enumerate-interval 0 10)))
